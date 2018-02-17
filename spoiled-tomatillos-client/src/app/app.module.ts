092f2be3078008633b9d4322737dc68f7a8b942a
Lexy Brooks <lexylena@gmail.com>
2018-02-17 12:12:13 -0500

    Added playlist page.

--- spoiled-tomatillos-client/src/app/app.module.ts
+++ spoiled-tomatillos-client/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppRoutingModule } from './/app-routing.module';
 import { HelloComponent } from './hello/hello.component';
 import { HelloService } from './hello.service';
 import { NotificationsComponent } from './notifications/notifications.component';
+import { PlaylistComponent } from './playlist/playlist.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
-    NotificationsComponent
+    NotificationsComponent,
+    PlaylistComponent
   ],
   imports: [
     BrowserModule,
---
+++ spoiled-tomatillos-client/src/app/playlist/playlist.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+// import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-playlist',
+  templateUrl: './playlist.component.html',
+  styleUrls: ['./playlist.component.css']
+})
+export class PlaylistComponent implements OnInit {
+
+  private playlist;
+  private uid;
+  // private pages;
+
+  constructor(private route: ActivatedRoute) {
+    this.uid = this.route.snapshot.params.uid;
+  }
+
+  ngOnInit() {
+    this.getPlaylist();
+  }
+
+  getPlaylist() {
+    // this.userService.getUserPlaylist(this.uid)
+    //   .subscribe(playlist => this.playlist = playlist);
+    this.playlist = [
+      {id: 1, Title: "Shrek", Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"},
+      {id: 2, Title: "Requiem for a Dream", Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BOTdiNzJlOWUtNWMwNS00NmFlLWI0YTEtZmI3YjIzZWUyY2Y3XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"},
+      {id: 3, Title: "Superstar", Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BN2Q2YjFlMzYtYjVlMC00MmZmLTg5MzQtZjUyYmY2N2U5MjIxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"},
+      {id: 4, Title: "Goon", Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTcxOTQwMTQ3MF5BMl5BanBnXkFtZTcwMDcyOTQwNw@@._V1_SX300.jpg"},
+      {id: 5, Title: "Ex Machina", Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxNzc0OTIxMV5BMl5BanBnXkFtZTgwNDI3NzU2NDE@._V1_SX300.jpg"},
+    ];
+  }
+
+}
