1fa43a99f29d81a93f55ac367297298a1aeabb81
spozder <pozder.s@husky.neu.edu>
2018-02-17 15:09:54 -0500

    Added ESlint, including settings file, and fixed linting errors

--- spoiled-tomatillos-server/routes/api.js
+++ spoiled-tomatillos-server/routes/api.js
@@ -1,60 +1,59 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var mysql = require('mysql')
+const mysql = require('mysql');
 
 function query(q, onQ) {
-
-    var connection = mysql.createConnection({
-      host     : 'cs4500-spring2018-fishstein.cxjizgp7es7a.us-west-2.rds.amazonaws.com',
-      user     : 'ofishstein',
-      password : 'cs4500db',
-      database : 'cs4500_spring2018_fishstein'
-    });
-    connection.connect()
-    connection.query(q, onQ)
-    connection.end()
+  let connection = mysql.createConnection({
+    host     : 'cs4500-spring2018-fishstein.cxjizgp7es7a.us-west-2.rds.amazonaws.com',
+    user     : 'ofishstein',
+    password : 'cs4500db',
+    database : 'cs4500_spring2018_fishstein'
+  });
+  connection.connect();
+  connection.query(q, onQ);
+  connection.end();
 }
 
 
 
 /* GET hello string. */
-router.get('/hello/string', function(req, res, next) {
+router.get('/hello/string', function(req, res) {
   res.send('Hello, World!');
 });
 
 /* GET hello string. */
-router.get('/hello/object', function(req, res, next) {
-  r = {'message': 'Hello, World!'}
+router.get('/hello/object', function(req, res) {
+  let r = {'message': 'Hello, World!'};
   res.send(r);
 });
 
-router.post('/hello/insert', function(req, res, next) {
-    q = "INSERT INTO hello (id, message) VALUES (NULL,'Hello Team 22');"
-    query(q, function (err, rows, fields) {
-        if (err) {
-            console.log(q)
-            throw err;
-        }
-    })
-    res.send('Success!')
+router.post('/hello/insert', function(req, res) {
+  let q = 'INSERT INTO hello (id, message) VALUES (NULL,\'Hello Team 22\');';
+  query(q, function (err) {
+    if (err) {
+      console.log(q);
+      throw err;
+    }
+  });
+  res.send('Success!');
 });
 
 // Post a parameterized string to db
 router.post('/hello/insert/:msg', function(req, res) {
-    let msg = req.params.msg
-    q = "INSERT INTO hello (id, message) VALUES (NULL,'" + msg + "');"
-    query(q, function (err, rows, fields) {
-      if (err) throw err;
-    })
-    res.send("Success!")
+  let msg = req.params.msg;
+  let q = 'INSERT INTO hello (id, message) VALUES (NULL,\'' + msg + '\');';
+  query(q, function (err) {
+    if (err) throw err;
+  });
+  res.send('Success!');
 });
 
 router.get('/hello/select/all', function(req, res) {
-    query('SELECT * from hello;', function (err, rows, fields) {
-      if (err) throw err;
-      res.send(rows)
-    })
+  query('SELECT * from hello;', function (err, rows) {
+    if (err) throw err;
+    res.send(rows);
+  });
 });
 
 
