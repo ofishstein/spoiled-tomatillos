595b91e04c30a85b96287fbcd2e5b7c8e393e71e
Lexy Brooks <lexylena@gmail.com>
2018-02-19 12:28:40 -0500

    Restyle profile image display.

--- spoiled-tomatillos-client/src/app/users-list/users-list.component.ts
+++ spoiled-tomatillos-client/src/app/users-list/users-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import {userInfo} from "os";
 
 @Component({
   selector: 'app-users-list',
@@ -23,17 +24,40 @@ export class UsersListComponent implements OnInit {
 
   ngOnInit() {
     this.users = [
-      {userId: 124, username: "bob_lob_law", profileImageUrl: "../../assets/profile_image_placeholder.png", isFollowing: false},
-      {userId: 125, username: "fred124", profileImageUrl: "../../assets/profile_image_placeholder.png", isFollowing: true},
-      {userId: 126, username: "jane", profileImageUrl: "../../assets/profile_image_placeholder.png", isFollowing: false},
-      {userId: 127, username: "ann_23sdf", profileImageUrl: "../../assets/profile_image_placeholder.png", isFollowing: false},
-      {userId: 128, username: "shreksy_lexy", profileImageUrl: "../../assets/profile_image_placeholder.png", isFollowing: true}
+      {userId: 124, username: "bob_lob_law", profileImage: {
+        url: "../../assets/profile_image_placeholder.png", width: 167, height: 167
+      }, isFollowing: false},
+      {userId: 125, username: "fred124", profileImage: {
+        url: "http://lorempixel.com/400/400/", width: 400, height: 400
+      }, isFollowing: true},
+      {userId: 126, username: "jane", profileImage: {
+        url: "http://lorempixel.com/300/400/", width: 300, height: 400
+      }, isFollowing: false},
+      {userId: 127, username: "ann_23sdf", profileImage: {
+        url: "../../assets/profile_image_placeholder.png", width: 167, height: 167
+      }, isFollowing: false},
+      {userId: 128, username: "shreksy_lexy", profileImage: {
+        url: "http://lorempixel.com/750/500/", width: 750, height: 500
+      }, isFollowing: true}
     ];
     // if (this.listType == 'followers') {
     //   this.getFollowers();
     // } else {
     //   this.getFollowing();
     // }
+
+    this.users.forEach((user) => {
+      user.style = {};
+      if (user.profileImage.height >= user.profileImage.width) {
+        user.style.height = 200;
+        user.style.width = "auto";
+      } else {
+        console.log("else statemeent");
+        user.style.width = 200;
+        user.style.height = "auto";
+
+      }
+    });
   }
 
   /**
