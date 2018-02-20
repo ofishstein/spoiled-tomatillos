dcd88d93cf1b48af6bdc195e8f87c313a94d36ee
Lexy Brooks <lexylena@gmail.com>
2018-02-20 15:03:05 -0500

    Fixed types. Change double quotes to single quotes for strings.

--- spoiled-tomatillos-client/src/app/playlist/playlist.component.ts
+++ spoiled-tomatillos-client/src/app/playlist/playlist.component.ts
@@ -9,31 +9,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PlaylistComponent implements OnInit {
 
-  private playlist;
-  private uid;
-  // private pages;
+  private playlist: any;
+  private uid: number;
+  // private pages: number[];
 
   constructor(private route: ActivatedRoute) {
-    this.uid = this.route.snapshot.params.uid;
+    this.uid = parseInt(this.route.snapshot.params.uid);
   }
 
   ngOnInit() {
     this.getPlaylist();
   }
 
-  getPlaylist() {
+  getPlaylist(): void {
     // this.userService.getUserPlaylist(this.uid)
     //   .subscribe(playlist => this.playlist = playlist);
     this.playlist = [
-      {id: 1, Title: "Shrek", Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"},
-      {id: 2, Title: "Requiem for a Dream", Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BOTdiNzJlOWUtNWMwNS00NmFlLWI0YTEtZmI3YjIzZWUyY2Y3XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"},
-      {id: 3, Title: "Superstar", Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BN2Q2YjFlMzYtYjVlMC00MmZmLTg5MzQtZjUyYmY2N2U5MjIxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"},
-      {id: 4, Title: "Goon", Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTcxOTQwMTQ3MF5BMl5BanBnXkFtZTcwMDcyOTQwNw@@._V1_SX300.jpg"},
-      {id: 5, Title: "Ex Machina", Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxNzc0OTIxMV5BMl5BanBnXkFtZTgwNDI3NzU2NDE@._V1_SX300.jpg"},
+      {id: 1, Title: 'Shrek', Poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg'},
+      {id: 2, Title: 'Requiem for a Dream', Poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BOTdiNzJlOWUtNWMwNS00NmFlLWI0YTEtZmI3YjIzZWUyY2Y3XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg'},
+      {id: 3, Title: 'Superstar', Poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BN2Q2YjFlMzYtYjVlMC00MmZmLTg5MzQtZjUyYmY2N2U5MjIxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg'},
+      {id: 4, Title: 'Goon', Poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTcxOTQwMTQ3MF5BMl5BanBnXkFtZTcwMDcyOTQwNw@@._V1_SX300.jpg'},
+      {id: 5, Title: 'Ex Machina', Poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxNzc0OTIxMV5BMl5BanBnXkFtZTgwNDI3NzU2NDE@._V1_SX300.jpg'},
     ];
   }
 
-  removeFromPlaylist(movieId: number) : void {
+  removeFromPlaylist(movieId: number): void {
     // call service to remove given movie from user's playlist
   }
 
