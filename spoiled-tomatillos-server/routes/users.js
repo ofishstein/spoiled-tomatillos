e40986118f97bd4da5bcaba0d7c6a811ba1e00f7
Campbell Alden <alden.ca@husky.neu.edu>
2018-02-21 15:24:44 -0500

    Create an endpoint for creating a user
     - Add package-lock.json back into the project

--- spoiled-tomatillos-server/routes/users.js
+++ spoiled-tomatillos-server/routes/users.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../db/model/User');
+
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
+});
+
+router.post('/create', function(req, res) {
+// you can also build, save and access the object with chaining:
+  User
+  .build(req.params.user)
+  .save()
+  .then(() => {
+    res.sendStatus(200);
+  })
+  .catch(error => {
+    console.log(error)
+    res.sendStatus(500);
+  })
 });
 
 module.exports = router;
