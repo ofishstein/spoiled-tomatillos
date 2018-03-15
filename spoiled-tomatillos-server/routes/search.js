11c88b9db45997e5479cd8d4dc41fad58d386481
Campbell Alden <alden.ca@husky.neu.edu>
2018-03-15 01:33:19 -0400

    Search works but returns everything

--- spoiled-tomatillos-server/routes/search.js
+++ spoiled-tomatillos-server/routes/search.js
@@ -4,42 +4,34 @@ const router = express.Router();
 const db = require('../db/db.js');
 const session = db.get_session();
 
+
+function handleSearch(queries, model, Sequelize, callBack) {
+  searchWhere = {where: {[Sequelize.Op.or]: {}}}
+
+  for (var key in queries) {
+    searchWhere['where'][Sequelize.Op.or][key] = {
+        [Sequelize.Op.like]: '%' + queries[key] + '%'
+    };
+  }
+
+  model.findAll(searchWhere)
+  .then(results => {
+    callBack(results);
+  });
+}
+
 /* GET users listing. */
 router.get('/movies', function(req, res) {
-  reqStrings = "%" + req.query.q + "%"
-  console.log(reqStrings)
-  response = {}
-  session.Movie
-    .findAll({
-      where: {
-        title: {
-          [session.Sequelize.Op.like]: reqStrings
-        }
-      },
-    })
-    .then(movies => {
-      res.send(movies)
-    });
+  handleSearch(req.query, session.Movie, session.Sequelize, (results) => {
+    res.send(results);
+  });
 });
 
 
 router.get('/users', function(req, res) {
-  session.User
-    .findAll({
-      where: {
-        [session.Sequelize.Op.or]: {
-          username: {
-            [session.Sequelize.Op.like]: reqStrings
-          },
-          email: {
-            [session.Sequelize.Op.like]: reqStrings
-          }
-        }
-      }
-    })
-    .then(users => {
-      res.send(users)
-    })
+  handleSearch(req.query, session.User, session.Sequelize, (results) => {
+    res.send(results);
+  });
 });
 
 module.exports = router;
