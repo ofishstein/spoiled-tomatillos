2e3214a76089e6ad1051d665ca2a2864666289dc
Oli Fishstein <ofishstein@gmail.com>
2018-03-14 22:12:07 -0400

    add service to component

--- spoiled-tomatillos-client/src/app/account-settings/account-settings.component.ts
+++ spoiled-tomatillos-client/src/app/account-settings/account-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-account-settings',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  constructor() { }
+  public user: User = {
+      username: null,
+      email: null,
+      password: null,
+      firstName: null,
+      lastName: null,
+      isAdmin: false
+    };
+
+  constructor(private userService: UsersService) { }
 
   ngOnInit() {
+    this.userService.getUserInfo().subscribe((userData) => {
+      this.user.username = userData.username;
+      this.user.email = userData.email;
+      this.user.firstName = userData.firstName;
+      this.user.lastName = userData.lastName;
+      this.user.isAdmin = userData.isAdmin;
+    });
   }
 
 }
