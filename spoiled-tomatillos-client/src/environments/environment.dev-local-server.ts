461787b70696e2bb7d98d61fcbdbec3adf9f3c6c
Lexy Brooks <lexylena@gmail.com>
2018-03-15 13:53:31 -0400

    Added dev_local_server environment (see usage below). Run the server locally with 'npm start'. Run the client side with 'ng serve --environment=dev_local_server'. Note: Environment now contains serverBaseUri, so for all API calls, import environment and use environment.serverBaseUri, and this will be the appropriate string based on the given environment.

---
+++ spoiled-tomatillos-client/src/environments/environment.dev-local-server.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  envName: 'dev_local_server',
+  serverBaseUri: 'localhost:3000'
+};
--- spoiled-tomatillos-client/src/environments/environment.ts
+++ spoiled-tomatillos-client/src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  envName: 'dev',
+  serverBaseUri: 'http://ec2-18-216-146-141.us-east-2.compute.amazonaws.com:3000'
 };
