6c15954a1146547aabc853bd3744f2b8832f21b9
Lexy Brooks <lexylena@gmail.com>
2018-03-20 07:05:35 -0400

    Renamed 'dev_local_server' env to 'dev_local' and fixed base route.

--- spoiled-tomatillos-client/src/environments/environment.dev-local-server.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  envName: 'dev_local_server',
-  serverBaseUri: 'localhost:3000'
-};
---
+++ spoiled-tomatillos-client/src/environments/environment.dev_local.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  envName: 'dev_local',
+  serverBaseUri: 'http://localhost:3000'
+};
