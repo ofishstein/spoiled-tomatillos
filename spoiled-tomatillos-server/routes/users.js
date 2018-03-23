b24543b376529b9537e7df5a344ff486bf566e4c
Campbell Alden <alden.ca@husky.neu.edu>
2018-03-23 15:45:04 -0400

    Create endpoint stubs for each new user endpoint

--- spoiled-tomatillos-server/routes/users.js
+++ spoiled-tomatillos-server/routes/users.js
@@ -7,7 +7,18 @@ const authCheck = require('./auth');
 
 /* GET user.
  * Return the profile information for the user at the given id.*/
-router.get('/:user_id', function(req, res) {
+router.get('/', authCheck, function(req, res) {
+  // TODO handle searching here.
+});
+
+router.get('/settings', authCheck, function(req, res) {
+  let u = req.user.get({plain: true});
+  delete u.password;
+  res.send(u);
+});
+
+
+router.get('/:user_id', authCheck, function(req, res) {
   session.User
     .findAll({
       where: {id: req.params['user_id']},
@@ -24,11 +35,27 @@ router.get('/:user_id', function(req, res) {
 
 });
 
-router.get('/:user_id/settings', authCheck, function(req, res) {
-  let u = req.user.get({plain: true});
-  delete u.password;
-  res.send(u);
+router.get('/:user_id/following', authCheck, function(req, res) {
+  // TODO: Get users that the user at the id follows
 });
+
+router.get('/:user_id/followers', authCheck, function(req, res) {
+  // TODO: Get users that the user at the id is followed by
+});
+
+router.get('/:user_id/is-following', authCheck, function(req, res) {
+  // TODO: Is the logged in user following the user at the given ID.
+});
+
+router.get('/:user_id/watchlists', authCheck, function(req, res) {
+  // TODO: Get users watchlist
+});
+
+router.get('/:user_id/reviews', authCheck, function(req, res) {
+  // TODO: Get users reviews
+});
+
+
 
 
 /* PUT user */
@@ -39,7 +66,25 @@ router.put('/:user_id', authCheck, function(req, res) {
   });
 });
 
-router.post('/create', function(req, res) {
+router.put('/settings', authCheck, function(req, res) {
+  //TODO: Handle putting user settings.
+});
+
+router.put('/:user_id/follow', authCheck, function(req, res) {
+  //TODO: Current logged in user follows user at user_id
+});
+
+router.put('/:user_id/unfollow', authCheck, function(req, res) {
+  //TODO: Current logged in user unfollows user at user_id
+});
+
+router.delete('/:user_id'. authCheck, function(req, res) {
+  // TODO: delete the user iff user_id == logged in user or logged in user is admin
+});
+
+
+// Post a user to the db. Admin only
+router.post('/', function(req, res) {
 // you can also build, save and access the object with chaining:
   session.User
     .build(req.body)
