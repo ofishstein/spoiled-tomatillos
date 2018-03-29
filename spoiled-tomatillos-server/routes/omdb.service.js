14ab3d77b75bc961594b5517742b5ae64ec98b5e
Lexy Brooks <lexylena@gmail.com>
2018-03-29 09:20:46 -0400

    Fixed OMDB methods

--- spoiled-tomatillos-server/routes/omdb.service.js
+++ spoiled-tomatillos-server/routes/omdb.service.js
@@ -1,55 +1,71 @@
 const db = require('../db/db');
 const session = db.get_session();
 
-const http = require('http');
+const request = require('request');
 const app = require('../app');
-const apiKey = app.omdbApiKey;
+// TODO: move to process env
+const apiKey = '3f811d1c';
 
 const omdbBase = 'http://www.omdbapi.com/?apikey=' + apiKey + '&';
 
 let omdb = {};
 
+/**
+ * OMDB responses can return regular errors (like 401, etc.),
+ * or, if some query param is incorrect (such as the id is incorrect),
+ * the response will be a 200 of the format
+ *      {Response: 'False', Error: 'error message...'}
+ */
+
 // Search movies by keyword and return json results of given page
-omdb.searchByKeyword = (keyword, page) => {
-    http.get(omdbBase + 's=' + keyword + '&page=' + page)
-        .then(movies => {
-            return movies;
-        });
+omdb.searchByKeyword = (keyword, page, callback) => {
+    request.get(omdbBase + 's=' + keyword + '&page=' + page, {json: true},
+        (err, res, body) => {
+        if (err) {
+            return callback({Error: err});
+        }
+        return callback(body);
+    });
 };
 
 // Get a movie by imdb id and return json results
 // if save is true, save the poster to db (checked if null in original db query)
-omdb.getMovieById = (id, save) => {
-    http.get(omdbBase + 'plot=full&i=' + id)
-        .then(movie => {
-            if (save) {
-                session.Movie.update(
-                    { poster: movie.poster },
-                    {
-                        where: { imdbId: id }
-                    })
-                    .then(ret => {
-                        return movie;
-                    });
-            } else {
-                return movie;
-            }
-        });
-};
-
-// Get a movie's poster image by imdb id and save to db
-omdb.getPosterById = (id) => {
-    http.get(omdbBase + 'i=' + id)
-        .then(movie => {
+omdb.getMovieById = (id, save, callback) => {
+    let movie;
+    // TODO: are all imported movielens imdb ID's 6 digits? (determine omdb prefix)
+    request.get(omdbBase + 'plot=full&i=tt0' + id, {json: true},
+        (err, res, body) => {
+        if (err) {
+            return callback({Error: err});
+        }
+        if (save) {
             session.Movie.update(
-                { poster: movie.poster },
+                { poster: body.Poster },
                 {
                     where: { imdbId: id }
                 })
-                .then(ret => {
-                    return movie.poster;
-                });
-        });
+                .then(ret => { });
+        }
+        movie = body;
+        return callback(body);
+    });
 };
 
-module.exports.omdb = () => omdb;
+// Get a movie's poster image by imdb id and save to db
+omdb.getPosterById = (id, callback) => {
+    request.get(omdbBase + 'i=tt0' + id, {json: true},
+        (err, res, body) => {
+        if (err) {
+            return callback({Error: err});
+        }
+        session.Movie.update(
+            { poster: body.Poster },
+            {
+                where: { imdbId: id }
+            })
+            .then(ret => { });
+        return callback(body.Poster);
+    });
+};
+
+module.exports = omdb;
