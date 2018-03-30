be0b4e16bb278fcc24ea349b2741d359a8bd0e39
spozder <pozder.s@husky.neu.edu>
2018-03-29 20:30:54 -0400

    Added test environment support, fixed existing login test to use new data

--- spoiled-tomatillos-server/test/login.test.js
+++ spoiled-tomatillos-server/test/login.test.js
@@ -1,15 +1,29 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app');
+const db = require('../db/db');
+const session = db.get_session();
+const testData = require('./testData');
 const should = chai.should();
 
 chai.use(chaiHttp);
 describe('Login', () => {
+  before((done) => {
+    session.User.bulkCreate(testData.users).then(() => {done();});
+  });
+
+  after((done) => {
+    const userIds = testData.users.map((user) => user.id);
+    session.User.destroy({where: {id: userIds}}).then(() => {
+      done();
+    });
+  });
+
   describe('POST Valid Login', () => {
     it('It should return a valid user', (done) => {
       let validUser = {
-        username: 'SeedUser1',
-        password: 'su1'
+        username: 'test_user1',
+        password: 'test'
       };
       chai.request(app).post('/api/login').send(validUser).end((err, res) => {
         res.should.have.status(200);
@@ -32,8 +46,8 @@ describe('Login', () => {
   describe('POST Invalid Login: Wrong Password', () => {
     it('It should return a 401 Unauthorized Error', (done) => {
       let invalidUser = {
-        username: 'SeedUser1',
-        password: 'su2'
+        username: 'test_user1',
+        password: 'fu1'
       };
       chai.request(app).post('/api/login').send(invalidUser).end((err, res) => {
         res.should.have.status(401);
