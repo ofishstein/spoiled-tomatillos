ef685b5a681000466cb37ede9d61531eaf1cd2a7
spozder <pozder.s@husky.neu.edu>
2018-03-31 13:09:43 -0400

    Fixed sonarqube bug

--- spoiled-tomatillos-server/routes/omdb.service.js
+++ spoiled-tomatillos-server/routes/omdb.service.js
@@ -19,52 +19,51 @@ let omdb = {};
 
 // Search movies by keyword and return json results of given page
 omdb.searchByKeyword = (keyword, page, callback) => {
-    request.get(omdbBase + 's=' + keyword + '&page=' + page, {json: true},
-        (err, res, body) => {
-        if (err) {
-            return callback({Error: err});
-        }
-        return callback(body);
+  request.get(omdbBase + 's=' + keyword + '&page=' + page, {json: true},
+    (err, res, body) => {
+      if (err) {
+        return callback({Error: err});
+      }
+      return callback(body);
     });
 };
 
 // Get a movie by imdb id and return json results
 // if save is true, save the poster to db (checked if null in original db query)
 omdb.getMovieById = (id, save, callback) => {
-    let movie;
-    // TODO: are all imported movielens imdb ID's 6 digits? (determine omdb prefix)
-    request.get(omdbBase + 'plot=full&i=tt0' + id, {json: true},
-        (err, res, body) => {
-        if (err) {
-            return callback({Error: err});
-        }
-        if (save) {
-            session.Movie.update(
-                { poster: body.Poster },
-                {
-                    where: { imdbId: id }
-                })
-                .then(ret => { });
-        }
-        movie = body;
-        return callback(body);
+  let movie;
+  // TODO: are all imported movielens imdb ID's 6 digits? (determine omdb prefix)
+  request.get(omdbBase + 'plot=full&i=tt0' + id, {json: true},
+    (err, res, body) => {
+      if (err) {
+        return callback({Error: err});
+      }
+      if (save) {
+        session.Movie.update(
+          { poster: body.Poster },
+          {
+            where: { imdbId: id }
+          })
+          .then(() => { });
+      }
+      return callback(body);
     });
 };
 
 // Get a movie's poster image by imdb id and save to db
 omdb.getPosterById = (id, callback) => {
-    request.get(omdbBase + 'i=tt0' + id, {json: true},
-        (err, res, body) => {
-        if (err) {
-            return callback({Error: err});
-        }
-        session.Movie.update(
-            { poster: body.Poster },
-            {
-                where: { imdbId: id }
-            })
-            .then(ret => { });
-        return callback(body.Poster);
+  request.get(omdbBase + 'i=tt0' + id, {json: true},
+    (err, res, body) => {
+      if (err) {
+        return callback({Error: err});
+      }
+      session.Movie.update(
+        { poster: body.Poster },
+        {
+          where: { imdbId: id }
+        })
+        .then(ret => { });
+      return callback(body.Poster);
     });
 };
 
