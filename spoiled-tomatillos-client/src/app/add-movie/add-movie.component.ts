92a587f1ba089c3a29a921af526475e391b48d77
Oli Fishstein <ofishstein@gmail.com>
2018-04-09 13:25:20 -0400

    add movie working

--- spoiled-tomatillos-client/src/app/add-movie/add-movie.component.ts
+++ spoiled-tomatillos-client/src/app/add-movie/add-movie.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminMovie } from '../admin-movie';
+import { Router } from '@angular/router';
+import { MovieService } from '../services/movie/movie.service';
 
 @Component({
   selector: 'app-add-movie',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddMovieComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  movie: AdminMovie = {
+    poster: null,
+    title: null,
+    tmdbId: null,
+    imdbId: null
+  };
+
+  constructor(private _router: Router, private _movieService: MovieService) {
+  }
 
   ngOnInit() {
   }
 
+  addMovie() {
+    this.loading = true;
+    this._movieService.addMovie(this.movie).subscribe(
+      data => {
+        this._router.navigate(['/admin/home']);
+      },
+      error => {
+        console.log(error);
+        this.loading = false;
+      }
+    );
+  }
 }
