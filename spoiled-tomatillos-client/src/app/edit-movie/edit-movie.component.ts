4cd6b6d6ea0ef83b19ba310c9611c3e339b8a832
Oli Fishstein <ofishstein@gmail.com>
2018-04-09 14:35:14 -0400

    get edit movie working

--- spoiled-tomatillos-client/src/app/edit-movie/edit-movie.component.ts
+++ spoiled-tomatillos-client/src/app/edit-movie/edit-movie.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminMovie } from '../admin-movie';
+import { MovieService } from '../services/movie/movie.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-movie',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditMovieComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  private movieData;
+
+  movie: AdminMovie = {
+    poster: null,
+    title: null,
+    tmdbId: null,
+    imdbId: null
+  };
+
+  constructor(private _router: Router, private _movieService: MovieService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this._movieService.getMovie(this.route.snapshot.params.id).subscribe(
+      data => {
+        console.log(data);
+        this.movieData = data;
+        this.movie.poster = this.movieData.poster;
+        this.movie.title = this.movieData.title;
+        this.movie.tmdbId = this.movieData.tmdbId;
+        this.movie.imdbId = this.movieData.imdbId;
+       },
+      err => console.error(err)
+    );
+  }
+
+  editMovie() {
+    this.loading = true;
+    this._movieService.editMovie(this.movie, this.route.snapshot.params.id).subscribe(
+      data => {
+        this._router.navigate(['/admin/home']);
+      },
+      error => {
+        console.log(error);
+        this.loading = false;
+      }
+    );
   }
 
 }
