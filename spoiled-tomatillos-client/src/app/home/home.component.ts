44c1528c7e939452b6a1ce36e9b5b9adf0bd8672
Oli Fishstein <ofishstein@gmail.com>
2018-04-10 17:14:40 -0400

    fix issues

--- spoiled-tomatillos-client/src/app/home/home.component.ts
+++ spoiled-tomatillos-client/src/app/home/home.component.ts
@@ -9,13 +9,19 @@ import { RecommendService } from '../services/recommend.service';
 export class HomeComponent implements OnInit {
 
   public recommendations;
+  public noRes: boolean;
 
-  constructor(private _recommendService: RecommendService) { }
+  constructor(private _recommendService: RecommendService) {
+      this.noRes = true;
+   }
 
   ngOnInit() {
     this._recommendService.getAllRecommendations().subscribe(
       data => {
         this.recommendations = data;
+        this.noRes = data.length === 0;
+        console.log(data);
+        console.log(data.length === 0);
       }, err => console.error(err)
     );
   }
