b082107d88eadf1aba9ba253a2ec855b60e21e90
Lexy Brooks <lexylena@gmail.com>
2018-04-10 08:01:40 -0400

    Add notifications table. Add hook to create notification after creating follower and recommendation.

--- spoiled-tomatillos-server/db/models/follower.js
+++ spoiled-tomatillos-server/db/models/follower.js
@@ -1,6 +1,23 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Follower = sequelize.define('Follower', {}, {});
+  const Follower = sequelize.define('Follower', {}, {
+    hooks:
+      {
+        afterCreate: (followship, options) => {
+          sequelize.models.Notification
+            .findOrCreate({
+              where: {
+                type: 'FOLLOWER',
+                userId: followship.followeeId,
+                seen: null,
+                recommendationId: null,
+                followshipId: followship.id
+              }
+            })
+            .then(() => {});
+        }
+      }
+  });
   Follower.associate = function(models) {
     Follower.belongsTo(models.User,
       {as: 'FollowerUser', foreignKey: 'followerId', sourceKey: 'id'});
--- spoiled-tomatillos-server/db/models/recommendation.js
+++ spoiled-tomatillos-server/db/models/recommendation.js
@@ -2,7 +2,24 @@
 module.exports = (sequelize, DataTypes) => {
   const Recommendation = sequelize.define('Recommendation', {
     message: DataTypes.STRING
-  }, {});
+  }, {
+    hooks:
+      {
+        afterCreate: (rec, options) => {
+          sequelize.models.Notification
+            .findOrCreate({
+              where: {
+                type: 'RECOMMENDATION',
+                userId: rec.recommendeeId,
+                seen: null,
+                recommendationId: rec.id,
+                followshipId: null
+              }
+            })
+            .then(() => {});
+        }
+      }
+  });
   Recommendation.associate = function(models) {
     Recommendation.belongsTo(models.User,
       {as: 'Recommender', sourceKey: 'id', foreignKey: 'recommenderId'});
